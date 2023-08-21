3eb5b7ddc44c5d8437ed79741d1286a9e76367d8
MarcelFeo <marcelfeo29@gmail.com>
2023-08-21 09:02:43 -0300

    adicionando tabela com os primos

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from utils import show_primes
 import ecc
 
 # PRIMOS UTILIZADOS NO EXPERIMENTO
@@ -14,9 +15,8 @@ prime3 = 2**256 - 2**224 + 2**192 + 2^96 - 1
 # (4) 384 bits: 2^384 - 2^128 - 2^96 + 2^32 - 1
 prime4 = 2**384 - 2**128 - 2^96 + 2^32 - 1
 
-# 521 bits: 2^521 - 1
-prime4 = 2**521 - 1
+# (5) 521 bits: 2^521 - 1
+prime5 = 2**521 - 1
 
-# ec = ecc.EllipticCurve(2, 7, 19)
-# ec.print_points()
-# print(ec.number_points())
+# TABELA COM OS VALORES DOS PRIMOS
+show_primes(prime1, prime2, prime3, prime4, prime5)
---
+++ utils.py
@@ -0,0 +1,15 @@
+from tabulate import tabulate
+
+def show_primes(p1, p2, p3, p4, p5):
+    data = [
+        ["192 bits", "2^192 - 2^64 - 1", "{:.5e}".format(p1)],
+        ["224 bits", "2^224 - 2^96 + 1", "{:.5e}".format(p2)],
+        ["256 bits", "2^256 - 2^224 + 2^192 + 2^96 - 1", "{:.5e}".format(p3)],
+        ["384 bits", "2^384 - 2^128 - 2^96 + 2^32 - 1", "{:.5e}".format(p4)],
+        ["521 bits", "2^521 - 1", "{:.5e}".format(p5)]
+    ]
+
+    header = ["Name", "Expression", "Value"]
+
+    table = tabulate(data, headers=header, tablefmt="grid")
+    print(table)
